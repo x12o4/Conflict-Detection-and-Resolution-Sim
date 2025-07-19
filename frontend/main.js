c076701d392df7d6e561fd6ca003b9dbcdbb2284
Ethan <kylsia2008@gmail.com>
2025-07-19 16:44:50 +0100

    fix icon variables, create array for storage of aircraft objects, create function to update Aircraft

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,6 @@
 
 // init map
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 3); 
 
 // Create tile layer with openstreetmap 
 
@@ -11,9 +11,25 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var AeroplaneIcon = L.icon({
     iconUrl: 'ICONS/airplane.png', // sets image for the aeroplane
-    iconSize: [38, 38], // size of the icon
+    iconSize: [30, 30], // size of the icon
+    iconAnchor: [15, 15] // centers the icon on long, lat coordinates.
 
 })
 
+const aircraftMarkers = {}; // used to store active aircraft markers
+
+// updates the aircraft positions
+function updateAircraftMarkers(aircraftData){
+    aircraftData.forEach(aircraft => {
+        const{id, lat,lon,heading} = aircraft;
+    })
+}
+
+const iconHeading = L.icon({
+    icon: 'ICONS/airplane.png', // refresh icon on map
+    iconSize: [30, 30], 
+    iconAnchor: [15, 15], 
+    
+})
 var marker = L.marker([51.5, -0.09], { icon: AeroplaneIcon }).addTo(map);
 
