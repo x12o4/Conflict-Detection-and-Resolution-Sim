530c7e6fe1cb5390e20b3b5a78496631f24397f8
Ethan <kylsia2008@gmail.com>
2025-07-20 16:38:05 +0100

    initialise flask and bluesky-simulator connection using pip install, initialise flask webserver, initialise bluesky sim, create error handling for simulation, create bluesky api route through localhost to retrieve live aircraft data, create GET_aircraft function to retrieve currently active aircraft

--- NEA/backend/backend.py
+++ NEA/backend/backend.py
@@ -1,7 +1,26 @@
 from flask import Flask, jsonify  # Flask web framework for building APIs, jsonify for converting python data to JSON
 from bluesky import simulation  # bluesky flight simulation model
 
-backend = Flask(__name__)  # creates flask webserver
-sim = simulation.Sim()  # creates a bluesky simulation object
+application = Flask(__name__)  # creates flask webserver
+simulation = simulation.Sim()  # creates a bluesky simulation  
 
+try: # try catch block to handle potential errors during simulation initialization
+  simulation = simulation.Sim() # init sim
+except Exception as e:
+  print(f"Error initializing simulation: {e}")
+  sim = None # fallback if simulation fails to initialize
+
+@application.route('/aircraft') # defines route for bluesky api to retrieve live aircraft data at localhost/aircraft
+
+def GET_aircraft(): # executes when /aircraft is accessed
+  if not simulation: # check if simulation is initialized
+    return jsonify({"error": "Simulation is not initialized!"}), 500  # return error if simulation is not initialized
+    
+    simulation.step() # advances the simulation by a timestep which makes the aircraft move to the next position
+    return jsonify([{ # converts to JSON to send to the frontend
+        "id": aircraft.id,
+        "lat": aircraft.lat,
+        "lon": aircraft.lon,
+        "heading": ac.heading
+    } for aircraft in sim.get_aircraft()]) # retrieves all active aircraft and extracts their id, latitude, longitude, and heading
 
