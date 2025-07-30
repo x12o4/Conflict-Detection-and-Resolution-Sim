c1ce0a399094af2f22ba6d704af09c3e0bf2d779
Ethan <kylsia2008@gmail.com>
2025-07-30 15:45:30 +0100

    import time, math and priority queue (heapq), create simAirspace class which includes methods to control the airspace, implement DetectConflicts method which adds aircraft to a priority queue

--- NEA/backend/backend.py
+++ NEA/backend/backend.py
@@ -6,7 +6,10 @@ import requests # used to make HTTP requests, pip install requests
 from threading import Lock # used to handle multiple requests simultaneously without conflicts, so that a request has to finish before another is made
 import traceback # used for more detailed debugging
 from flask_cors import CORS # used to allow cross-origin requests, so that the frontend can access the backend API from a different domain or port, pip install flask-cors
-import math
+import math 
+from math import exp # exp used for exponential calculations
+import time
+import heapq # used for priority queue implementation
 
 application = Flask(__name__)  # creates flask webserver
 
@@ -104,10 +107,80 @@ class Aircraft:
             "verticalspeed": self.verticalspeed
         }
 
+class simAirspace:
 
+    def __init__(self):
+        self.aircraft: dict[str, Aircraft] = {}  # dictionary to store aircraft objects with callsign as key
+        self.LOCK = Lock()  # lock to ensure thread safety when updating aircraft data
+        self.startTime = time.time()  # records the start time of the simulation
 
+    def addAircraft(self, aircraft: Aircraft):
+        with self.LOCK:
+            self.aircraft[aircraft.callsign] = aircraft # adds aircraft to the dictionary
+    
+    def removeAircraft(self, aircraft: Aircraft):
+        with self.LOCK:
+            if aircraft.callsign in self.aircraft:
+                del self.aircraft[aircraft.callsign]
+
+    def updateAirspace(self, time):
+        with self.LOCK:
+            for aircraft in self.aircraft.values():
+                aircraft.updateAircraftPosition(time)  # updates the position of each aircraft in the airspace
+    def getAircraftData(self):
+        with self.LOCK:
+            return [aircraft.dataToJsonDictionary() for aircraft in self.aircraft.values()] # returns a list of aircraft data in JSON format
+    
+      # minimum separation distance in kilometers, set to 5 nautical miles
+     # minimum altitude difference in feet, set to 1000 feet
+
+    # priority queue implementation
+    def DetectConflicts(self,  minimumSeperationDistanceKM: float = nmToKM * 5,
+     minimumAltitudeDifferenceFT: float = 1000.0):
+
+     conflict = [] # list to store conflicts
+     aircraftList = list(self.aircraft.values())  # converts the dictionary of aircraft to a list 
+
+    # use of 2 for loops means the function becomes O(N^2) in time complexity, n being the number of aircraft in the airspace
+    # might need to be optimised if the number of aircraft increases 
+     for i in range(len(aircraftList)): 
+        for j in range(i + 1, len(aircraftList)): # iterate through the aircraft list, starting from the next aircraft to avoid comparing the same aircraft with itself
+            # compares each aircraft with every other aircraft to find conflicts
+            aircraft1 = aircraftList[i] #
+            aircraft2 = aircraftList[j]
+            distanceKM = aircraft1.position.distancefrom(aircraft2.position) # calculates distance between two aircraft using the distancefrom subroutine in Position class     
+            altitudeDifferenceFT = abs(aircraft1.alt - aircraft2.alt) # calculates altitude difference between two aircraft, abs used to get the absolute value
+
+            # checks if the distance and altitude difference are below the minimum standard for aeroplanes by ICAO standards
+            if distanceKM < minimumSeperationDistanceKM and altitudeDifferenceFT < minimumAltitudeDifferenceFT:
+                # used to determine the place in the priority queue, the closer to 1 means the higher the risk of colliding
+                horizontalRisk = 1 - (distanceKM / minimumSeperationDistanceKM)
+                verticalRisk = 1 - (altitudeDifferenceFT / minimumAltitudeDifferenceFT)
+
+                # placeholders until the actual function is implemented
+                speedRisk = self.calculateSpeedRisk(aircraft1, aircraft2)
+                timeToCollision = self.calculateTimeToCollision(aircraft1, aircraft2) 
+
+                # risk score ranging from 0 to 1, used to determine the place in the priority queue
+                riskScore = 0.4 * horizontalRisk + 0.4 * verticalRisk + 0.2 * speedRisk * 0.1 * (1 / (1 + exp(timeToCollision)))
+                
+
+                heapq.heappush(conflict, ( 
+                -riskScore, { # -riskScore is used to implement a maxheap (finding the highest priority first) as python's heapq  is a minheap (finds the lowest priority) by default
+                "aircraft1": aircraft1.callsign,
+                "aircraft2": aircraft2.callsign,
+                "distanceKM": distanceKM,
+                "altitudeDifferenceFT": altitudeDifferenceFT,
+                "riskScore": riskScore,
+                "timeToCollision": timeToCollision
+                    }
+                ))
+                
+
+                return [item[1] for item in heapq.nsmallest(len(conflict),conflict)] # returns conflicts sorted by risk score,  heapq.nsmallest(len(conflict),conflict)] returns items ordered asc, which because of the negative risk scores it # means the highest risk score is first, so we return the first item in the list, item[1] returns the second element in the tuple which is the dictionary 
 @application.route('/aircraft') # defines route for bluesky api to retrieve live aircraft data at localhost/aircraft
 @cache.cached(timeout=0.5)  # caches the response for 2 seconds to reduce server load
+
 
 def get_aircraft(): # executes when /aircraft is accessed
     global traffic
